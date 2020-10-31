9faad141f72516ca97745dfd8685ad5923f21ff7
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2020-10-31 13:41:37 +0000

    added move and remove to router

--- src/server/routes/v3/index.ts
+++ src/server/routes/v3/index.ts
@@ -5,10 +5,14 @@ const router = express.Router()
 import createDirectory from "./create-dir"
 import upload from "../v3/upload"
 import get from "../v3/get"
+import move from "../v3/move"
+import remove from "../v3/remove"
 
 // apply routes
 router.use("/create-dir", createDirectory)
 router.use("/upload", upload)
 router.use("/get", get)
+router.use("/move", move)
+router.use("/remove", remove)
 
 export default router
