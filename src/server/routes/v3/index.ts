546ab05b337454914c16f180499749c7392b7aaa
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2020-10-31 11:52:14 +0000

    added all routes

--- src/server/routes/v3/index.ts
+++ src/server/routes/v3/index.ts
@@ -4,9 +4,11 @@ const router = express.Router()
 // import routes
 import createDirectory from "./create-dir"
 import upload from "../v3/upload"
+import get from "../v3/get"
 
 // apply routes
 router.use("/create-dir", createDirectory)
 router.use("/upload", upload)
+router.use("/get", get)
 
 export default router
