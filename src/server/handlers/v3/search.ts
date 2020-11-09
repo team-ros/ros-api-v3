ebb625e8234525cb838e93805cba4f9dd0aa38f1
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2020-11-09 21:35:53 +0000

    added Search response serializer

--- src/server/handlers/v3/search.ts
+++ src/server/handlers/v3/search.ts
@@ -1,7 +1,20 @@
 import { Query } from "mongoose"
 import { SearchClient } from "../../../elasticsearch/connection"
 
-export const SearchObject = async (query: string, owner: String) => {
+interface IRawSearch {
+    _id: string
+    _index: string
+    _score: number
+    _type: string
+    _source: {
+        id: string
+        name: string
+        owner: string
+        type: boolean
+    }
+}
+
+export const SearchObject = async (query: string, owner: String): Promise<IRawSearch[] | false> => {
     try {
         const response = await SearchClient.search({
             index: String(process.env.ELASTIC_INDEX),
@@ -47,4 +60,15 @@ export const SearchObject = async (query: string, owner: String) => {
         if(Boolean(process.env.DEV)) console.error(err)
         return false
     }
+}
+
+export const SerializeResponse = (search: IRawSearch[]) => {
+    return search.map(value => {
+        return {
+            score: value._score,
+            id: value._source.id,
+            name: value._source.name,
+            type: value._source.type ? "file" : "directory"
+        }
+    })
 }
