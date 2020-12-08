dffc354db5f50a9135f3d0b48194213583ed9c62
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2020-12-08 11:57:56 +0000

    removed middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,18 +5,18 @@ const app = express()
 // import middleware
 import cors from "cors"
 import bodyParser from "body-parser"
-import { firebaseMiddleware } from "./middleware/firebaseMiddleware"
 
 // apply middleware
 app.use(cors())
 app.use(bodyParser.json())
-app.use(firebaseMiddleware)
 
 // import routes
 import v3 from "./routes/v3"
+import admin from "./routes/admin"
 
 // apply routes
 app.use("/v3", v3)
+app.use("/admin", admin)
 
 // set http port
 app.listen(process.env.PORT || 8080)
